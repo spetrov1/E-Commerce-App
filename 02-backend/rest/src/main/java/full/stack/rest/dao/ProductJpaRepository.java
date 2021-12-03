7079ac7a4f170e96ec59f28d52e9836897407d1d
spetrov1 <svetoslav.petrov@lights.digital>
2021-12-03 16:44:08 +0200

    Backend Version 2.1 (synched with Frontend)
    Add custom query method for selecting products by category id

--- 02-backend/rest/src/main/java/full/stack/rest/dao/ProductJpaRepository.java
+++ 02-backend/rest/src/main/java/full/stack/rest/dao/ProductJpaRepository.java
@@ -1,9 +1,15 @@
 package full.stack.rest.dao;
 
 import full.stack.rest.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
 }
