704414f63bbf51377974468351f2d38e0795bc3f
spetrov1 <svetoslav.petrov@lights.digital>
2021-12-04 00:38:07 +0200

    Version 2.3
    Add findByNameContaining method

--- 02-backend/rest/src/main/java/full/stack/rest/dao/ProductJpaRepository.java
+++ 02-backend/rest/src/main/java/full/stack/rest/dao/ProductJpaRepository.java
@@ -12,4 +12,6 @@ public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
 }
